8af3fc82340968e4651507f3371b77f5e649d89e
closer <37889723+closer@users.noreply.github.com>
2018-12-14 18:23:02 +0800

    2018.12.14 update

---
+++ test 14/14.6.c
@@ -0,0 +1,120 @@
+/*6.一个文本文件中保存着一个垒球队的信息。每行数据都是这样排列：
+4 Jessie Joybat 5 2 1 1
+第1项是球员号，为方便起见，其范围是0～18。第2项是球员的名。第3
+项是球员的姓。名和姓都是一个单词。第4项是官方统计的球员上场次数。
+接着3项分别是击中数、走垒数和打点（RBI）。文件可能包含多场比赛的
+数据，所以同一位球员可能有多行数据，而且同一位球员的多行数据之间可
+能有其他球员的数据。编写一个程序，把数据储存到一个结构数组中。该结
+构中的成员要分别表示球员的名、姓、上场次数、击中数、走垒数、打点和
+安打率（稍后计算）。可以使用球员号作为数组的索引。该程序要读到文件
+结尾，并统计每位球员的各项累计总和。
+世界棒球统计与之相关。例如，一次走垒和触垒中的失误不计入上场次
+数，但是可能产生一个RBI。但是该程序要做的是像下面描述的一样读取和
+处理数据文件，不会关心数据的实际含义。
+要实现这些功能，最简单的方法是把结构的内容都初始化为零，把文件
+中的数据读入临时变量中，然后将其加入相应的结构中。程序读完文件后，
+应计算每位球员的安打率，并把计算结果储存到结构的相应成员中。计算安
+打率是用球员的累计击中数除以上场累计次数。这是一个浮点数计算。最
+后，程序结合整个球队的统计数据，一行显示一位球员的累计数据。*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#define LEN 20      //字符长度
+#define SIZE 20     //球员个数
+
+struct player{
+    int num; //球员号
+    char first_name[LEN];
+    char last_name[LEN];
+    int action;//上场次数
+    int hit;//击中数
+    int walk;//走垒数
+    int rbi;//打点
+    float average;//安打率
+};
+void a_get_zero(struct player *man,int n);//数据变0
+void b_get_data(struct player *man,int n);//获取数据
+void c_print(struct player *man,int n);//打印
+
+int main(void)
+{
+    static struct player players[SIZE];
+    a_get_zero(players,SIZE);
+    b_get_data(players,SIZE);
+    c_print(players,SIZE);
+    printf("再见.");
+
+    return 0;
+}
+
+void a_get_zero(struct player *man,int n)
+{
+    for(int i;i<n;i++)//清零
+        man[i]=(struct player){0,"","",0,0,0,0,0};
+}
+
+void b_get_data(struct player *man,int n)
+{
+    FILE *p;
+    char name[LEN];
+    int nu,ac,hi,wa,rb;
+    char f_name[LEN],l_name[LEN];
+
+
+    puts("输入你要打开的文件名：");
+    gets(name);
+    if(( p = fopen(name,"rb")) == NULL)
+    {
+        fprintf(stderr,"不能打开%s文件",name);
+        exit(EXIT_FAILURE);
+    }
+    while( (fscanf(p,"%d%s%s%d%d%d%d",&nu,f_name,
+                   l_name,&ac,&hi,&wa,&rb)) == 7)
+    {//读取文件数据叠加存储到变量
+        man[nu].num = nu;
+        if(man[nu].first_name[0] =='\0')
+        {
+            strcpy(man[nu].first_name,f_name);
+            strcpy(man[nu].last_name,l_name);
+        }
+        man[nu].action += ac;
+        man[nu].hit += hi;
+        man[nu].walk += wa;
+        man[nu].rbi += rb;
+    }
+	if (fclose(p) != 0)
+		fprintf(stderr,"关闭文件时发生错误.\n");
+}
+
+void c_print(struct player *man,int n)
+{
+    int i=0;
+    int ac=0,hi=0,wa=0,rb=0;
+
+    for(i=0;i<n;i++)
+    {//计算安打率
+        man[i].average=man[i].hit /(float)man[i].action;
+    }
+    printf("\n");
+    printf("球员号\t名\t姓\t场次\t击中数\t走垒数"
+            "\t打点\t安打率\n");
+    for(i=0;i<n;i++)
+    {
+        if(man[i].first_name[0] !='\0')
+            {
+            printf("%d\t%s\t%s\t%d\t%d\t%d\t%d\t%.3g\n",
+                   man[i].num,man[i].first_name,man[i].last_name,
+                   man[i].action,man[i].hit,man[i].walk,man[i].rbi,
+                   man[i].average);
+            }
+           ac +=man[i].action;
+           hi +=man[i].hit;
+           wa +=man[i].walk;
+           rb +=man[i].rbi;
+    }
+    printf("\n");
+    printf("总计: 场次 %d\t击中数 %d\t走垒数 %d\t打点 %d\t平均安打率 %.3g\n",
+            ac,hi,wa,rb,hi/(float)ac);
+}
+
---
+++ test 14/14.7.c
@@ -0,0 +1,217 @@
+/*7.修改程序清单 14.14，从文件中读取每条记录并显示出来，允许用户
+删除记录或修改记录的内容。如果删除记录，把空出来的空间留给下一个要
+读入的记录。要修改现有的文件内容，必须用"r+b"模式，而不是"a+b"模
+式。而且，必须更加注意定位文件指针，防止新加入的记录覆盖现有记录。
+最简单的方法是改动储存在内存中的所有数据，然后再把最后的信息写入文
+件。跟踪的一个方法是在book结构中添加一个成员表示是否该项被删除。*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+#define LEN 20
+#define MAXTITL 40
+#define MAXAUTL 40
+#define MAXBOOKS 10     // 最大书籍数量
+int count = 0;      //外部变量，书的数目
+struct book {
+    char title[MAXTITL];
+    char author[MAXAUTL];
+    float value;
+};
+
+char * s_gets(char * st, int n);		//输入字符串的函数
+char get_choice(void);					//获取用户选择
+void add(struct book li[]);				//添加若干本书
+void change(struct book *li);				//替换一本书
+void del(struct book *li);				//删除一本书
+void print(struct book *li);				//打印书单
+void write_file(struct book *li, char *name);	//将数据写入文件
+
+int main(void)
+{
+    struct book library[MAXBOOKS];
+    char ch,name[LEN];
+    FILE * fp;
+
+    puts("输入你要打开的文件名：");
+    s_gets(name,LEN);
+    if ((fp = fopen(name, "r+b")) == NULL)
+    {
+        fprintf(stderr,"不能打开%s文件",name);
+        exit(EXIT_FAILURE);
+    }
+    rewind(fp);     // 定位到文件开始
+    while (count < MAXBOOKS && fread(&library[count],
+            sizeof(struct book), 1, fp) == 1)
+    {
+        count++;//统计当前书本数目
+    }
+    fclose(fp);
+
+    print(library);//打印已有书籍列表
+    printf("现有书籍%d本.\n",count);
+
+    //选择清单
+    while( (ch=get_choice()) != 'q')
+    {
+        switch(ch)
+        {
+        case 'a':add(library);
+                break;
+        case 'b':change(library);
+                break;
+        case 'c':del(library);
+                break;
+        case 'd':print(library);
+                break;
+        default:puts("程序错误.");
+            break;
+        }
+    }
+
+    //数据写入文件
+    write_file(library,name);
+    puts("再见.");
+
+    return 0;
+}
+
+char * s_gets(char * st, int n)
+{
+    char * ret_val;
+    char * find;
+
+    ret_val = fgets(st, n, stdin);
+    if (ret_val)
+    {
+        find = strchr(st, '\n'); // 查找换行符
+        if (find)       // 如果地址不是 NULL，
+            *find = '\0';    // 在此处放置一个空字符
+        else
+            while (getchar() != '\n')
+                continue;   // 清理输入行
+    }
+    return ret_val;
+}
+
+char get_choice(void)   //获取用户选择
+{
+    char ch;
+
+    puts("\n请输入你的选择：");
+    printf("a:增\t\tb:改\n"
+           "c:删\t\td:查\n"
+           "q:保存和离开\n");
+    while(( ch = (tolower(getchar())) ) != 'a' && ch != 'b' &&
+          ch != 'c' && ch != 'd' && ch != 'q') //转小写字母
+    {
+        while(getchar() !='\n') continue;//清理输入行
+        puts("请输入正确的字母.");
+    }
+    while(getchar() !='\n') continue;
+
+    return ch;
+}
+
+void add(struct book li[])
+{
+    if(count == MAXBOOKS)
+        puts("皇上,臣妾实在装不下了.\n");
+    else
+    {
+        puts("请输入书名(按[enter]离开).");
+        while (count < MAXBOOKS && s_gets(li[count].title, MAXTITL) != NULL
+                && li[count].title[0] != '\0')
+        {
+            puts("请输入作者.");
+            s_gets(li[count].author, MAXAUTL);
+            puts("请输入价格.");
+            scanf("%f", &li[count++].value);
+            while (getchar() != '\n')
+                continue;    // 清理输入行
+            if (count < MAXBOOKS)
+                puts("输入下一个书名(按[enter]回到选择界面).");
+        }
+    }
+}
+
+void change(struct book *li)
+{
+    int i;
+    char title[MAXTITL];
+
+    puts("请输入你需要替换信息的书籍名：");
+    s_gets(title,MAXTITL);
+    for(i=0;i<count;i++)
+    {
+        if( strcmp(title,li[i].title) == 0)//检测输入书名和文件中书名是否一致
+        {
+            puts("变更后的书籍名字是？");
+            s_gets(li[i].title, MAXTITL);
+            puts("请输入作者.");
+            s_gets(li[i].author, MAXAUTL);
+            puts("请输入价格.");
+            scanf("%f", &li[i].value);
+            while (getchar() != '\n')
+                continue;    // 清理输入行
+            break;
+        }
+    }
+    if(i==count)
+        puts("找不到你要更改的书籍!\n");
+}
+
+void del(struct book *li)
+{
+    int i,j;
+    int flag=0;//标记是否找到
+    char title[MAXTITL];
+
+    if(count == 0)
+        puts("皇上，臣妾这里没有书!\n");
+    puts("请输入你需要删除的书籍名：");
+    s_gets(title,MAXTITL);
+    for(i=0;i<count;i++)
+    {
+        if( ! strcmp(title,li[i].title) )
+        {//检测输入书名和文件中书名是否一致
+            flag=1;
+            for(j=i;j<count;j++)//一致则后面的数据变动
+                li[j]=li[j+1];//后面的数据提到前面
+            count--;
+            //最后一条数据置零
+            strcpy(li[count].title, "");
+            strcpy(li[count].author, "");
+            li[count].value = 0;
+            break;
+        }
+    }
+    if(!flag)
+        puts("找不到你要删除的书籍!\n");
+}
+
+void print(struct book *li)
+{
+    int index;
+    if (count > 0)
+    {
+        puts("\n书籍清单如下:");
+        for (index = 0; index < count; index++)
+            printf("%s by %s: $%.2f\n", li[index].title,
+                    li[index].author, li[index].value);
+        printf("\n");
+    }
+    else
+        puts("没有找到书,真遗憾!\n");
+
+}
+
+void write_file(struct book *li, char *name)  //将数据写入文件
+{
+    FILE *p;
+
+    p=fopen(name,"w+b");
+    fwrite(li, sizeof(struct book), count,p);
+    fclose(p);
+}
